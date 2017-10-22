f42d271d9c927e88b98eff3b90ce4e8dd75ef0c1
Mason Hall <masonhall@gmail.com>
2017-10-21 21:52:44 -0500

    Full functionality with standard inputs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var alexa = require('alexa-sdk');
 
 
-const pi = '1415926535';
+const pi = '141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609433057270365759591953092186117381932611793105118548074462379962749567351885752724891227938183011949129833673362440656643086021394946395224737190702179860943702770539217176293176752384674818467669405132000568127145263560827785771342757789609173';
 const HELP_MESSAGE = "I'm trying to help you learn the digits of pi. Give me your best guess and I'll help you along.";
 const WELCOME_MESSAGE = "Welcome to Pi Hard. We help you learn the digits of pi.";
 const END_MESSAGE = "Thanks for playing. We hoped you learned some digits of pi.";
@@ -29,6 +29,9 @@ const handlers = {
 var startHandlers = alexa.CreateStateHandler(states.START, {
     "Start":function(){
         this.emit(":ask", WELCOME_MESSAGE);
+        this.handler.state = states.GUESS;
+        this.emitWithState("Guess");
+        
     },
     "PlayIntent": function (){
         this.handler.state = states.GUESS;
@@ -47,10 +50,13 @@ var guessHandlers = alexa.CreateStateHandler(states.GUESS, {
     "Guess": function (){
         this.emit(":ask","Tell me the digits of pi that you know.");
         this.emit("GuessIntent");
+        console.log("made it to here");
     },
     "GuessIntent": function () {
-
+        
+        console.log("Got to guess intent");
         var answerSlotValid = isAnswerSlotValid(this.event.request.intent);
+        
         console.log(answerSlotValid);
         if (answerSlotValid){
             var valueString = answerSlotValid.toString();
@@ -63,6 +69,7 @@ var guessHandlers = alexa.CreateStateHandler(states.GUESS, {
                 if (valueString.charAt(i) !== pi.charAt(i)){
                     correct = false;
                     incorrectDigit = i + 1;
+                    
                     break;
                 }
             }
@@ -70,7 +77,7 @@ var guessHandlers = alexa.CreateStateHandler(states.GUESS, {
             console.log(correct);
             console.log(valueLength);
         
-
+            
             var responseString = "";
             if(correct){
                 var nextThree = '';
@@ -83,13 +90,14 @@ var guessHandlers = alexa.CreateStateHandler(states.GUESS, {
                 responseString = "Nice try. Unfortunately, you replaced "
                 + pi.charAt(incorrectDigit-1) + " with " + valueString.charAt(incorrectDigit-1);
             }
-            responseString = responseString + ". <break time='2s'/>  Do you want to play again?";
+            responseString = responseString + ". <break time='1s'/>  Do you want to play again?";
             this.emit(":ask", responseString);
             this.emit(":responseReady");
         }
 
     },
     "AMAZON.YesIntent": function () {
+
         this.emitWithState("Guess");
     },
     "AMAZON.NoIntent": function () {
