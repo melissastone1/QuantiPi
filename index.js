4620753cc8b5f2378222ef19fcea344342cb805e
Mason Hall <masonhall@gmail.com>
2017-10-21 23:41:18 -0500

    Final bug fixes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /* Written by Mason and Melissa */
 var alexa = require('alexa-sdk');
 
-
 const pi = '141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609433057270365759591953092186117381932611793105118548074462379962749567351885752724891227938183011949129833673362440656643086021394946395224737190702179860943702770539217176293176752384674818467669405132000568127145263560827785771342757789609173';
 const HELP_MESSAGE = "I'm trying to help you learn the digits of pi. Give me your best guess and I'll help you along.";
-const WELCOME_MESSAGE = "Welcome to Pi Hard. We help you learn the digits of pi.";
+const WELCOME_MESSAGE = "Welcome to Pi Hard. We help you learn the digits of pi. Say lets start learning to begin.";
 const END_MESSAGE = "Thanks for playing. We hoped you learned some digits of pi.";
-
+const UNHANDLED_START = "Just say Lets start learning to make a guess! ";
+const UNHANDLED_GUESS = "You did not give a valid guess. Do you want to try again?";
 var states = {
     START: "_START",
     GUESS: "_GUESS"
@@ -22,7 +22,7 @@ const handlers = {
         this.emit(":responseReady");
     },
     "Unhandled": function (){
-        this.emit(':ask', HELP_MESSAGE,HELP_MESSAGE);
+        this.emit(':ask', "Unhandled handlers",HELP_MESSAGE);
     }
 };
 
@@ -42,7 +42,7 @@ var startHandlers = alexa.CreateStateHandler(states.START, {
         this.emit(":responseReady");
     },
     "Unhandled": function (){
-        this.emit(':ask', HELP_MESSAGE,HELP_MESSAGE);
+        this.emit(':ask', UNHANDLED_START,HELP_MESSAGE);
     }
 });
 
@@ -52,6 +52,10 @@ var guessHandlers = alexa.CreateStateHandler(states.GUESS, {
         this.emit("GuessIntent");
         console.log("made it to here");
     },
+    "OneDigitIntent" : function(){
+        this.emit(":ask","Thats not a good guess! Pi starts with 3 point 1. Do you want to try again?")
+    },
+    
     "GuessIntent": function () {
         
         console.log("Got to guess intent");
@@ -93,8 +97,9 @@ var guessHandlers = alexa.CreateStateHandler(states.GUESS, {
             responseString = responseString + ". <break time='1s'/>  Do you want to play again?";
             this.emit(":ask", responseString);
             this.emit(":responseReady");
+        } else{
+            this.emit(":ask",UNHANDLED_GUESS);
         }
-
     },
     "AMAZON.YesIntent": function () {
 
@@ -104,12 +109,11 @@ var guessHandlers = alexa.CreateStateHandler(states.GUESS, {
         this.emit(":tell",END_MESSAGE);
     },
     'AMAZON.HelpIntent': function(){
-        this.emit(":ask", HELP_MESSAGE,HELP_MESSAGE);
+        this.emit(":ask", HELP_MESSAGE);
         this.emit(":responseReady");
     },
     "Unhandled": function (){
-        this.emit(':ask', HELP_MESSAGE,HELP_MESSAGE);
-        console.log("unhandled-guessHandlers2")
+        this.emit(':ask',UNHANDLED_GUESS);
     }
 });
 
@@ -123,7 +127,6 @@ function isAnswerSlotValid(intent){
         return false;
     }
 }
-
 
 // Connect to lambda
 exports.handler = function(event, context) {
